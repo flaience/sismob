335feab629805909e961dd52398fab3995335a5c
luismollinetti <69463960+luismollinetti@users.noreply.github.com>
2026-04-02 21:34:36 -0300

    fix: to railway

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,27 +3,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-// Variável para fazer cache da instância do servidor e economizar recursos
-let cachedApp: any;
-
 async function bootstrap() {
-  if (!cachedApp) {
-    const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-    // Habilita CORS para o seu frontend Next.js
-    app.enableCors();
-    app.useGlobalPipes(new ValidationPipe({ transform: true }));
-
-    // Inicializa o NestJS sem travar a porta
-    await app.init();
-
-    cachedApp = app.getHttpAdapter().getInstance();
-  }
-  return cachedApp;
+  // O Railway vai injetar a porta automaticamente
+  const port = process.env.PORT || 3000;
+  await app.listen(process.env.PORT || 3005);
+  console.log(`🚀 Sismob API rodando na porta ${port}`);
 }
-
-// O segredo da Vercel: exportar como um Default Export
-export default async (req: any, res: any) => {
-  const app = await bootstrap();
-  return app(req, res);
-};
+bootstrap();
