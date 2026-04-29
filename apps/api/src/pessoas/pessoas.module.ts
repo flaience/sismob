740636e951da08892dfef9ab4ac818e9640f9978
luismollinetti <69463960+luismollinetti@users.noreply.github.com>
2026-04-29 13:03:57 -0300

    montagem das telas de manutenção - 1.0.24 = 13:02  29/04

--- apps/api/src/pessoas/pessoas.module.ts
+++ apps/api/src/pessoas/pessoas.module.ts
@@ -1,8 +1,10 @@
+//src/pessoas/pessoas.module.ts
 import { Module } from '@nestjs/common';
 import { PessoasController } from './pessoas.controller';
 import { PessoasService } from './pessoas.service';
 import { AuthModule } from '../auth/auth.module'; // <--- Adicione este import
 
+// linha teste
 @Module({
   imports: [AuthModule], // <--- IMPORTANTE: Adicione o AuthModule aqui
   controllers: [PessoasController],
