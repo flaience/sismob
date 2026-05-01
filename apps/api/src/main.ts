d0c07d1b1715682d54244d35f6e426a9cae3c700
luismollinetti <69463960+luismollinetti@users.noreply.github.com>
2026-05-01 12:12:41 -0300

    montagem das telas de manutenção - 1.0.53 = 12:12  01/05

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,16 +6,19 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // 1. LIBERAÇÃO TOTAL DE CORS (Para não haver mais erro 404/Timeout no prefetch)
+  // LIBERAÇÃO TOTAL DE FRONTEIRA (Mata o erro de CORS)
   app.enableCors({
-    origin: true, // Aceita qualquer origem (Vercel, localhost, etc)
+    origin: true, // Aceita qualquer site (Vercel, Localhost, etc)
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
   });
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
   const port = process.env.PORT || 3005;
+
+  // 0.0.0.0 é vital para o Railway
   await app.listen(port, '0.0.0.0');
 
   console.log(`🚀 Sismob API rodando na porta: ${port}`);
