66975324cad3edb4dbbd7eb2edda2a4e93e19a81
luismollinetti <69463960+luismollinetti@users.noreply.github.com>
2026-05-01 21:31:51 -0300

    montagem das telas de manutenção - 1.0.63 = 21:31  01/05

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,19 +5,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
-
-  // 1. Criamos o app
   const app = await NestFactory.create(AppModule);
 
-  // 2. CORS total para não termos erros mentirosos
   app.enableCors({ origin: '*', credentials: true });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  // 3. A PORTA QUE O RAILWAY QUER
-  const port = process.env.PORT || 3000;
+  // O Railway vai injetar 3005 aqui se você configurou na variável
+  const port = process.env.PORT || 3005;
 
-  // 4. O SEGREDO: O listen deve ser a última coisa, mas ele não pode travar.
-  // Usamos '0.0.0.0' para o Railway enxergar o container.
+  // 🚨 O TIRO DE MISERICÓRDIA NO 502:
+  // Você PRECISA passar '0.0.0.0'. Se deixar vazio, ele usa 'localhost'
+  // e o Railway não consegue falar com o container.
   await app.listen(port, '0.0.0.0');
 
   logger.log(`🚀 SERVIDOR SISMOB ATIVO NA PORTA: ${port}`);
